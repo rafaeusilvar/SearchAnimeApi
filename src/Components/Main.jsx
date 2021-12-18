c3294836d38b42df8cd12797ad40358ddb992fe5
Rafael <rafaelsilvar74@gmail.com>
2021-12-18 14:28:06 -0300

    att

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,7 +4,7 @@ import './Main.css'
 
 const Main = () => {
   return ( 
-    <p>Anime Experience é uma plataforma de descoberta de anime moderna que ajuda você a rastrear o anime que está assistindo, descobrir novos anime e socializar com outros fãs.</p>
+    <p>Anime Experience é uma plataforma de descoberta de anime moderna que ajuda você a rastrear o anime que está assistindo, descobrir novos anime e socializar com outros fãs. </p>
    );
 }
 
