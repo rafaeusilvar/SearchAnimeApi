97f486468a1a9ded4f98dfa91579fc3bd7052e14
Rafael <rafaelsilvar74@gmail.com>
2021-12-19 18:58:07 -0300

    att

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -12,7 +12,7 @@ export default function Carousel() {
     <MDBCarousel showIndicators showControls fade>
       <MDBCarouselInner>
         <MDBCarouselItem className='active'>
-          <MDBCarouselElement src='https://initiate.alphacoders.com/images/183/cropped-1366-768-183434.jpg?7500' alt='...' /> 
+          <MDBCarouselElement src='https://images7.alphacoders.com/105/thumb-1920-1054068.png' alt='...' /> 
           <MDBCarouselCaption>
             <h5>Anime Experience</h5>
             <p>uma plataforma de descoberta de anime moderna que ajuda você a rastrear o anime que está assistindo.</p>
@@ -30,8 +30,6 @@ export default function Carousel() {
         <MDBCarouselItem>
           <MDBCarouselElement src='https://initiate.alphacoders.com/images/744/cropped-1366-768-744315.png?6882' alt='...' />
           <MDBCarouselCaption>
-            <h5></h5>
-            <p></p>
           </MDBCarouselCaption>
         </MDBCarouselItem>
       </MDBCarouselInner>
